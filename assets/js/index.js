d857e454aa0cfcf654921628572d88036f8319b8
ramiporta <p0rt4csd@gmail.com>
2022-04-23 15:17:52 -0300

    btn "Ver mas / Ver menos" mobile

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+const btnMas = document.querySelector(".btn-mas");
+let btnState = true;
+btnMas.addEventListener("click", ()=>{
+    const products = document.querySelectorAll(".product");
+    if (btnState){
+        products.forEach(prod =>{
+            prod.classList.remove("d-none");
+        });
+        btnMas.textContent = "VER MENOS";
+        btnState = false;
+    }else{
+        products.forEach((prod, i) =>{
+            if (i>2) prod.classList.add("d-none");
+        });
+        btnMas.textContent = "VER MÁS";
+        btnState = true;
+    }
+});
